2d0b07de9eea1337fb5be93124112ef7cae8beb4
jonatan-c <jona.cc.93@gmail.com>
2022-03-30 13:39:54 -0300

    initial proyect

---
+++ src/main/java/com/relacionunomuchosjavaspringboot/librosbiblioteca/LibrosBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.relacionunomuchosjavaspringboot.librosbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relacionunomuchosjavaspringboot/librosbiblioteca/LibrosBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relacionunomuchosjavaspringboot.librosbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
